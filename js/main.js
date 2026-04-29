5c4a67b6074cbc4eb94d80e20fd080ee1af676d5
chunilov <carlosojeda0587@gmail.com>
2026-04-28 23:47:40 -0300

    DFVGFD

--- js/main.js
+++ js/main.js
@@ -243,3 +243,30 @@ function activarCategorias() {
     });
   });
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const input = document.getElementById("inputBusqueda");
+
+  if (!input) return;
+
+  input.addEventListener("input", () => {
+    const texto = input.value.toLowerCase();
+
+    const nominados = document.querySelectorAll(".nominado");
+
+    nominados.forEach(nom => {
+      const nombre = nom.dataset.nombre?.toLowerCase() || "";
+      const categoria = nom.dataset.categoria?.toLowerCase() || "";
+
+      const coincide =
+        nombre.includes(texto) ||
+        categoria.includes(texto);
+
+      if (coincide) {
+        nom.style.display = "block";
+      } else {
+        nom.style.display = "none";
+      }
+    });
+  });
+});
